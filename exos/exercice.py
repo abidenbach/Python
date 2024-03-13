4945ee3537945f6d53c3518771959c577062cfd0
abidenbach <antoine.bidenbach1@gmail.com>
2024-03-13 15:05:39 +0100

    initialisation avec creation fichier readme.md

---
+++ exos/exercice.py
@@ -0,0 +1,36 @@
+# Créé par Elève, le 26/01/2024 en Python 3.7
+
+#9.4
+##a = float(input('Saisir un nb a'))
+##print(a)
+##b = float(input('Saisir un nb b'))
+##print(b)
+##c = a
+##a = b
+##b = c
+##print('a=',a,"; b=",b)
+
+###9.5
+##nb = int(input('Nb entier à trois chiffres'))
+##if nb >= 100 and nb < 1000 :
+##    nb = str(nb)
+##    n1 = int(nb[0])
+##    n2 = int(nb[1])
+##    n3 = int(nb[2])
+##    print(n1+n2+n3)
+
+#9.7
+##n = int(input('Rentrer un nombre entier :'))
+##if n % 3 == 0 and n > 100 :
+##    print("C'est bon !")
+
+#9.9
+##somme = 0
+##for i in range(1,51):
+##    somme+= i^^2
+##    print(somme)
+
+#9.14
+##import random
+##alea = int(899.9*random.random()+100)
+##devine = int(input("Saisis un nombre aléatoire"))
